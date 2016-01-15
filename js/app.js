8340e13b655c1365573542d7a95ea42be23f68ad
wypa4ok <wupa4ok@gmail.com>
2016-01-15 18:58:48 -0500

    Initial Commit

--- js/app.js
+++ js/app.js
@@ -2,11 +2,17 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = getRandomInt(1,100);
+    this.y = getRandomInt(1,100);
+    this.speed = getRandomInt(1,100);;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
+
+var getRandomInt = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -14,6 +20,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x++;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,15 +28,41 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(){
+    
+    this.x = 100;
+    this.y = 100;
 
+    this.sprite = 'images/char-boy.png';
+}
 
+Player.prototype.update = function(dt){
+    //this.x*=dt;
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(input){
+    if(input == 'left') { this.x-=10; }
+    if(input == 'right') { this.x+=10; }
+    if(input == 'up') { this.y-=10; }
+    if(input == 'down') { this.y+=10; }
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var player = new Player();
+var allEnemies = [];
 
+for(var i=0; i<10; i++){
+    allEnemies.push(new Enemy());
+}
 
 
 // This listens for key presses and sends the keys to your
@@ -41,6 +74,5 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
--- js/engine.js
+++ js/engine.js
@@ -26,7 +26,7 @@ var Engine = (function(global) {
         lastTime;
 
     canvas.width = 505;
-    canvas.height = 606;
+    canvas.height = 506;
     doc.body.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
